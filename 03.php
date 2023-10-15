534cf5d252a0eb316615f6fc123826a4fea78ba2
ZakariyeYusuf09 <zakariyeyuusuf617@gmail.com>
2023-10-15 05:29:02 +0300

    removed unwanted

--- 03.php
+++ 03.php
@@ -7,17 +7,7 @@
 </head>
 <body>
 <?php
-// $num = 14;
-// while ($num > 0) {
-//     if ($num % 2 == 0) {
-//         echo $num . " is even\n";
-//     } else {
-//         echo $num . " is odd\n";
-//     }
-//     $num--;
-// }
 
-// or 
 $y = 6;
 
 while($y>0){
